551a61134caa9815d1a61a7eaf82246f5fcbfe29
Trina <92634994+trinayau@users.noreply.github.com>
2022-10-11 12:08:40 +0100

    added project structure for Fibonacci challenge

---
+++ Fibonacci/Fibonacci.java
@@ -0,0 +1,11 @@
+import java.util.*;
+/* Given a number N, we need to find the Fibonacci Series up to the N term. */
+
+public class Fibonacci{
+    public static int fib(int n){
+        if(n==1)
+    }
+    public static void main(String[] args) {
+
+    }
+}
