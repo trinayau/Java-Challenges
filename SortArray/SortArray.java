c056e05529add9f30b808510076d639d8bd195af
Trina <92634994+trinayau@users.noreply.github.com>
2022-10-12 11:57:10 +0100

    Add SortArray by asc or desc challenge

---
+++ SortArray/SortArray.java
@@ -0,0 +1,31 @@
+package SortArray;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import static java.util.Arrays.*;
+
+/* Sort array in ascending/descending order*/
+public class SortArray {
+
+    public static void sortArrayAsc(int[] arr){
+        Arrays.sort(arr);
+        System.out.println("Ascending: " + Arrays.toString(arr));
+    }
+    public static void sortArrayDesc(int[] arr){
+        ArrayList<Integer> intArr = new ArrayList<Integer>();
+        for(int num: arr){
+            intArr.add(num);
+        }
+        Collections.reverse(intArr);
+        System.out.println("Descending:" + intArr.toString());
+
+
+    }
+    public static void main(String[] args) {
+        int[] test = {5,20,3,19};
+        sortArrayAsc(test);
+        sortArrayDesc(test);
+
+    }
+}
